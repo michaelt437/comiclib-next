96e0b748d36a1638658db10393e63bca9403a203
Michael Tran <michaelt437@gmail.com>
2021-08-04 00:12:43 -0400

    list grid: convert table to css grid

--- components/ComicsList/ComicsList.tsx
+++ components/ComicsList/ComicsList.tsx
@@ -17,39 +17,47 @@ export default function ComicsList({ items }: { items: IComic[] }) {
     <div className="rounded-md p-6 row-span-1 col-span-full md:col-span-2">
       <div className="flex justify-between items-center flex-wrap mb-5 md:flex-nowrap">
         <h2 className="flex-shrink-0">Book List</h2>
-        <div className="flex-grow w-full md:max-w-lg md:ml-5">
+        <div className="flex-grow w-full rounded-md md:max-w-lg md:ml-5">
           <input
-            className="px-3 py-2 rounded-md w-full"
+            className="px-3 py-2 rounded-md w-full bg-gray-200 focus:bg-blueGray-50"
             type="text"
             placeholder="Search..."
           />
         </div>
       </div>
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Writer</th>
-            <th>Publisher</th>
-            <th className="text-center">Score</th>
-            <th className="text-center">Status</th>
-          </tr>
-        </thead>
-        <tbody>
+      <div className="grid-table">
+        <div className="grid-table_thead">
+          <div className="grid-table_row grid grid-cols-8">
+            <div className="grid-table_col col-span-4">Title</div>
+            <div className="grid-table_col">Writer</div>
+            <div className="grid-table_col">Publisher</div>
+            <div className="grid-table_col text-center">Score</div>
+            <div className="grid-table_col text-center">Status</div>
+          </div>
+        </div>
+        <div className="grid-table_tbody">
           {items.map((comic) => {
             return (
-              <tr key={comic.title} className="hover:bg-blueGray-50">
-                <td className="text-sky-700">{comic.title}</td>
-                <td>{comic.writer}</td>
-                {/* <td className={`text-${comic.publisher.toLowerCase()}`}> */}
-                <td>{comic.publisher}</td>
-                <td className="text-center text-sky-600">{comic.score}</td>
-                <td className="text-center">{statusIcon(comic.status)}</td>
-              </tr>
+              <div
+                className="grid-table_row grid grid-cols-8 hover:bg-blueGray-50"
+                key={comic.title}
+              >
+                <div className="grid-table_col col-span-4 text-sky-700">
+                  {comic.title}
+                </div>
+                <div className="grid-table_col">{comic.writer}</div>
+                <div className="grid-table_col">{comic.publisher}</div>
+                <div className="grid-table_col text-center text-sky-600">
+                  {comic.score}
+                </div>
+                <div className="grid-table_col text-center">
+                  {statusIcon(comic.status)}
+                </div>
+              </div>
             );
           })}
-        </tbody>
-      </table>
+        </div>
+      </div>
     </div>
   );
 }
