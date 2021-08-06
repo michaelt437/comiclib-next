850bf9624d8eb0bf1fbdc183465bd9ced15dfa53
Michael Tran <michaelt437@gmail.com>
2021-08-06 19:32:45 -0400

    lint i think

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,17 @@ import Layout from "../components/Layout";
 import Modal from "../components/Modal/Modal";
 import ComicsList from "../components/ComicsList/ComicsList";
 import { createClient } from "@supabase/supabase-js";
-const supabase = createClient("https://wwwhlgyjoggvjxbbycgn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODEwMDUwNCwiZXhwIjoxOTQzNjc2NTA0fQ.C2zMG2-CyTumjZPJWEKymbsYIHuCjyJpgK9lMj8euRY");
+const supabase = createClient(
+  "https://wwwhlgyjoggvjxbbycgn.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODEwMDUwNCwiZXhwIjoxOTQzNjc2NTA0fQ.C2zMG2-CyTumjZPJWEKymbsYIHuCjyJpgK9lMj8euRY"
+);
 
-export default function Home () {
+export default function Home() {
   const [library, setLibrary] = useState<IComic[]>([]);
   const [openModal, setOpenModal] = useState<boolean>(false);
 
   useEffect(() => {
-    async function fetchBooks (): Promise<void> {
+    async function fetchBooks(): Promise<void> {
       const { data, error } = await supabase.from("comicbooks").select("*");
       setLibrary(data as IComic[]);
     }
@@ -20,9 +23,14 @@ export default function Home () {
 
   return (
     <Layout>
-      {openModal ? <Modal changeModalState={(val: boolean) => setOpenModal(val)} /> : null}
-      <ComicsList items={library} changeModalState={(val: boolean) => setOpenModal(val)} />
-      <div className="flex flex-col rounded-md p-6 col-span-auto">
+      {openModal ? (
+        <Modal changeModalState={(val: boolean) => setOpenModal(val)} />
+      ) : null}
+      <ComicsList
+        items={library}
+        changeModalState={(val: boolean) => setOpenModal(val)}
+      />
+      <div className="flex flex-col rounded-md p-6 col-span-auto border border-gray-300">
         <h2 className="mb-5">Stats</h2>
         <div className="flex flex-col flex-grow justify-center items-center">
           <p className="text-5xl">{library.length}</p>
