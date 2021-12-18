f0d931074c97fb16013b5c4dcb4e36f3642390b0
Michael Tran <michaelt437@gmail.com>
2021-12-18 17:05:50 -0500

    tailwind: dont need to define sky and slate no mo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,10 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  media: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        "sky": {
-          50: colors.sky[50],
-          100: colors.sky[100],
-          200: colors.sky[200],
-          300: colors.sky[300],
-          400: colors.sky[400],
-          500: colors.sky[500],
-          600: colors.sky[600],
-          700: colors.sky[700],
-          800: colors.sky[800],
-          900: colors.sky[900]
-        },
-        "slate": {
-          50: colors.slate[50],
-          100: colors.slate[100],
-          200: colors.slate[200],
-          300: colors.slate[300],
-          400: colors.slate[400],
-          500: colors.slate[500],
-          600: colors.slate[600],
-          700: colors.slate[700],
-          800: colors.slate[800],
-          900: colors.slate[900]
-        },
         "marvel": colors.red[500],
         "dc": colors.blue[500],
         "vertigo": colors.gray[700],
