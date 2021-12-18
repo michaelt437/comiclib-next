e2359ecb2ac999112ffbd46b735a202a4069d4f0
Michael Tran <michaelt437@gmail.com>
2021-12-18 17:20:08 -0500

    tailwind: remove media option

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  media: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
