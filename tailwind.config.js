fda4fe82bcf1053f6a3c4494d646a915165b1556
Michael Tran <michaelt437@gmail.com>
2021-12-18 16:34:37 -0500

    tailwind: update blueGray to slate

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,17 +18,17 @@ module.exports = {
           800: colors.sky[800],
           900: colors.sky[900]
         },
-        "blueGray": {
-          50: colors.blueGray[50],
-          100: colors.blueGray[100],
-          200: colors.blueGray[200],
-          300: colors.blueGray[300],
-          400: colors.blueGray[400],
-          500: colors.blueGray[500],
-          600: colors.blueGray[600],
-          700: colors.blueGray[700],
-          800: colors.blueGray[800],
-          900: colors.blueGray[900]
+        "slate": {
+          50: colors.slate[50],
+          100: colors.slate[100],
+          200: colors.slate[200],
+          300: colors.slate[300],
+          400: colors.slate[400],
+          500: colors.slate[500],
+          600: colors.slate[600],
+          700: colors.slate[700],
+          800: colors.slate[800],
+          900: colors.slate[900]
         },
         "marvel": colors.red[500],
         "dc": colors.blue[500],
