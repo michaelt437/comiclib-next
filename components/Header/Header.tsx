e89741b7419eaa568148aac93db603304dd63ebe
Michael Tran <michaelt437@gmail.com>
2022-02-12 20:54:32 -0500

    header: add wishlist nav item

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+
 export default function Header () {
   return (
     <div className="font-bold bg-sky-600">
-      <div className="container">
+      <div className="container flex justify-between items-center text-gray-200">
         <h1 className="text-gray-200 text-xl">Comics Library</h1>
+        <nav>
+          <ul>
+            <li className="cursor-pointer">
+              <Link href="/wishlist">
+                <a>Wishlist</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
