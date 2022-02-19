e0325926526c37d4a1c68c63b3df1bc9d5d7b1b9
Michael Tran <michaelt437@gmail.com>
2022-02-19 10:31:18 -0500

    header: condition nav items

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header () {
+  const router = useRouter();
   return (
     <div className="font-bold bg-sky-600">
       <div className="container flex justify-between items-center text-gray-200">
@@ -8,9 +10,13 @@ export default function Header () {
         <nav>
           <ul>
             <li className="cursor-pointer">
-              <Link href="/wishlist">
-                <a>Wishlist</a>
-              </Link>
+              {router.pathname === "/" ? (
+                <Link href="/wishlist">
+                  <a>Wishlist</a>
+                </Link>
+              ) : (
+                <Link href="/">Comics List</Link>
+              )}
             </li>
           </ul>
         </nav>
