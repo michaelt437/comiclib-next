d1c2dc2f0445bc4fc1e531b28ebf2eda56e34223
Michael Tran <michaelt437@gmail.com>
2022-03-27 20:44:48 -0400

    index: lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-export default function Home({ libraryData }: { libraryData: Comicbook[] }) {
+export default function Home ({ libraryData }: { libraryData: Comicbook[] }) {
   const [library, setLibrary] = useState<Comicbook[]>([]);
   const [openAddModal, setOpenAddModal] = useState<boolean>(false);
   const [openEditModal, setOpenEditModal] = useState<boolean>(false);
@@ -35,14 +35,14 @@ export default function Home({ libraryData }: { libraryData: Comicbook[] }) {
   const [bookToDelete, setBookToDelete] = useState<Comicbook | null>(null);
 
   useEffect(() => {
-    async function fetchBooks(): Promise<void> {
+    async function fetchBooks (): Promise<void> {
       const { data, error } = await supabase
         .from("comicbooks")
         .select("*")
         .order("title", { ascending: true });
       setLibrary(data as Comicbook[]);
     }
-    async function checkAuth(): Promise<void> {
+    async function checkAuth (): Promise<void> {
       const user = (await supabase.auth.user()) as User;
       if (user && user.role) setAuthenticated(user.role === "authenticated");
     }
